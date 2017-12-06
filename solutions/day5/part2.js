84287af78d3902a5cc96122c617e9880fe16ec53
Joshua Pirie <joshuapirie@gmail.com>
2017-12-06 23:46:04 +1030

    completed day 5

---
+++ solutions/day5/part2.js
@@ -0,0 +1,17 @@
+require("fs").readFile(require("path").resolve(__dirname, "input"), "utf8", (err, data) => {
+    let steps = 0;
+    let index = 0;
+    let instructions = data.split(/\r?\n/).map(Number);
+    while(true) {
+        if(instructions[index] === undefined)
+            break;
+        let instruction = instructions[index];
+        if(instruction >= 3)
+            instructions[index]--;
+        else
+            instructions[index]++;
+        index += instruction;
+        steps++;
+    }
+    console.log(steps);
+});
