179c65727d5103f8353ac57981d4e86e15b0586d
Joshua Pirie <joshuapirie@gmail.com>
2017-12-07 00:10:03 +1030

    completed day 6

---
+++ solutions/day6/part1.js
@@ -0,0 +1,16 @@
+require("fs").readFile(require("path").resolve(__dirname, "input"), "utf8", (err, data) => {
+    let state = data.split("\t").map(Number);
+    let states = [];
+    while(true) {
+        states.push(state.toString());
+        let maxIndex = state.reduce((c, x, i, a) => x > a[c] ? i : c, 0);
+        let blocks = state[maxIndex];
+        state[maxIndex] = 0;
+        for(let i = 1; i <= blocks; i++)
+            state[(maxIndex + i) % state.length]++;
+
+        if(states.includes(state.toString()))
+            break;
+    }
+    console.log(states.length);
+});
