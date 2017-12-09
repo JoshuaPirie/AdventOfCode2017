95dcab27d8e48b250e7a3beeb385e211c70c46aa
Joshua Pirie <joshuapirie@gmail.com>
2017-12-09 15:56:40 +1030

    completed day 9

---
+++ solutions/day9/part2.js
@@ -0,0 +1,45 @@
+require("fs").readFile(require("path").resolve(__dirname, "input"), "utf8", (err, data) => {
+    let currLevel = 0;
+    let garbage = false;
+    let ignore = false;
+    let numGarbage = 0;
+    data.split("").forEach(x => {
+        switch(x) {
+            case "{":
+                if(!garbage && !ignore)
+                    currLevel++;
+                if(garbage && !ignore)
+                    numGarbage++;
+                ignore = false;
+                break;
+            case "}":
+                if(!garbage && !ignore)
+                    currLevel--;
+                if(garbage && !ignore)
+                    numGarbage++;
+                ignore = false;
+                break;
+            case "<":
+                if(garbage && !ignore)
+                    numGarbage++;
+                if(!ignore)
+                    garbage = true;
+                ignore = false;
+                break;
+            case ">":
+                if(!ignore)
+                    garbage = false;
+                ignore = false;
+                break;
+            case "!":
+                ignore = !ignore;
+                break;
+            default:
+                if(garbage && !ignore)
+                    numGarbage++;
+                ignore = false;
+                break;
+        }
+    });
+    console.log(numGarbage);
+});
