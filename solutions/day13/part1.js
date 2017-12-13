ee2216b72e6677ca3c7a91f9d6615f2f0b2bbdfc
Joshua Pirie <joshuapirie@gmail.com>
2017-12-13 16:03:43 +1030

    completed day 13

---
+++ solutions/day13/part1.js
@@ -0,0 +1,12 @@
+require("fs").readFile(require("path").resolve(__dirname, "input"), "utf8", (err, data) => {
+    let severity = 0;
+    let firewall = [];
+    data.split(/\r?\n/).forEach(x => {
+        let sides = x.split(": ").map(Number);
+        firewall[sides[0]] = sides[1];
+    });
+    for(let i = 0; i < firewall.length; i++)
+        if(firewall[i] !== undefined && i % ((firewall[i] - 1) * 2) == 0)
+            severity += i * firewall[i];
+    console.log(severity);
+});
