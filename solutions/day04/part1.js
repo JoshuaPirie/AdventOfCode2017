2943945068e4cbe234e390ff62840f7e00f292db
Joshua Pirie <joshuapirie@gmail.com>
2017-12-13 16:13:57 +1030

    refactored day 4

--- solutions/day04/part1.js
+++ solutions/day04/part1.js
@@ -1,15 +1,3 @@
 require("fs").readFile(require("path").resolve(__dirname, "input"), "utf8", (err, data) => {
-    let validPwds = 0;
-    data.split(/\r?\n/).forEach(pwd => {
-        let valid = true;
-        pwd.split(" ").forEach((e, i, a) => {
-            if(a.includes(e, i + 1)) {
-                valid = false;
-                return;
-            }
-        });
-        if(valid)
-            validPwds++;
-    });
-    console.log(validPwds);
+    console.log(data.split(/\r?\n/).reduce((acc, curr) => acc + !curr.split(" ").some((e, i, a) => a.includes(e, i + 1)), 0));
 });
--- solutions/day04/part2.js
+++ solutions/day04/part2.js
@@ -1,15 +1,3 @@
 require("fs").readFile(require("path").resolve(__dirname, "input"), "utf8", (err, data) => {
-    let validPwds = 0;
-    data.split(/\r?\n/).forEach(pwd => {
-        let valid = true;
-        pwd.split(" ").map(x => x.split('').sort().join('')).forEach((e, i, a) => {
-            if(a.includes(e, i + 1)) {
-                valid = false;
-                return;
-            }
-        });
-        if(valid)
-            validPwds++;
-    });
-    console.log(validPwds);
+    console.log(data.split(/\r?\n/).reduce((acc, curr) => acc + !curr.split(" ").map(x => x.split('').sort().join('')).some((e, i, a) => a.includes(e, i + 1)), 0));
 });
